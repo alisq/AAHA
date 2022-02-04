d6417bb1ab8ca832bc585305c41f92f4f4a2e664
alisq <ali.shamas.qadeer@gmail.com>
2022-02-04 12:26:28 -0500

    initial slide show updated.

--- js/main.js
+++ js/main.js
@@ -3,50 +3,58 @@ var gainst = document.getElementById('gainst');
 var ousing = document.getElementById('ousing');
 var lienation = document.getElementById('lienation');
 
+letterSpeed = 100;
+initialPause = 10000;
+completedPause = 3000;
 var typewriterRchiects = new Typewriter(rchitects, {
   cursor:'',
   loop: true,
-  delay: 75,
+  delay: letterSpeed,
 });
 
 typewriterGainst  = new Typewriter(gainst, {
   cursor:'',
   loop: true,
-  delay: 75,
+  delay: letterSpeed,
 });
 typewriterOusing  = new Typewriter(ousing, {
   cursor:'',
   loop: true,
-  delay: 75,
+  delay: letterSpeed,
 });
 typewriterLienation  = new Typewriter(lienation, {
   cursor:'',
   loop: true,
-  delay: 75,
+  delay: letterSpeed,
 });
 
 typewriterRchiects
-  .pauseFor(250)
+  .pauseFor(initialPause)
   .typeString('rchitects')
-  .pauseFor(3000)
+  .pauseFor(completedPause)
   .start();
 
 
   typewriterGainst
-  .pauseFor(250)
+  .pauseFor(initialPause)
   .typeString('gainst')
-  .pauseFor(3225)
+  .pauseFor(completedPause+(letterSpeed*3*2))
   .start();
 
   typewriterOusing
-  .pauseFor(250)
+  .pauseFor(initialPause)
   .typeString('ousing')
-  .pauseFor(3225)
+  .pauseFor(completedPause+(letterSpeed*3*2))
   .start();
 
   typewriterLienation
-  .pauseFor(250)
+  .pauseFor(initialPause)
   .typeString('lienation')
-  .pauseFor(3000)
+  .pauseFor(completedPause)
   .start();
 
+
+
+  $(".nav__button").click(function(){
+    $(document).scrollTo($(this).data("target"),500)
+  })
