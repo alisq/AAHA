660e4d2f9eaadc2944059ac509f9fba37f4e2fc8
alisq <ali.shamas.qadeer@gmail.com>
2022-02-24 15:15:49 -0500

    added more.

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ $(".campaign .close").click(function(){
   $("#campaigns").removeClass("active");
 })
 
-$(".campaign h2, .campaign h3").click(function(e){
+$(".campaign h2, .campaign h3, .campaign p, .campaign img").click(function(e){
   
   if (!$(this).parent().parent().hasClass("active")) {
       $(".campaign.active").removeClass("active");
